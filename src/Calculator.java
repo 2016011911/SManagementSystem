ec1f6c800ef7136e7e7e435ec1205875f9d8f29c
USER <USER@LAPTOP-4616M2T6>
2020-03-19 14:45:22 +0900

    new Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		double i,dval;
+		Scanner sc = new Scanner (System.in);
+		System.out.println ("È­¾¾ ¿Âµµ¸¦ ÀÔ·ÂÇÏ½Ã¿À.");
+		dval = sc.nextDouble ();
+		i = (dval - 32) * 5 / 9;
+		System.out.println ("¼·¾¾¿Âµµ = " + i);
+		
+		
+		
+	}
+
+
+
+}
