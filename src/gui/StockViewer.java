6a1ba8af9788c1d0d32974db6a04157c12099866
USER <USER@LAPTOP-4616M2T6>
2020-06-04 11:23:47 +0900

    11

--- src/gui/StockViewer.java
+++ src/gui/StockViewer.java
@@ -1,13 +1,18 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-public class StockViewer extends JFrame{
-
-	public StockViewer() {
+public class StockViewer extends JPanel{
+	
+	WindowFrame frame;
+	
+	public StockViewer(WindowFrame frame) {
+		
+		this.frame = frame;
 		DefaultTableModel model = new DefaultTableModel();
 		model.addColumn("Name");
 		model.addColumn("Price");
@@ -19,10 +24,7 @@ public class StockViewer extends JFrame{
 		JScrollPane sp = new JScrollPane(table);
 		
 		this.add(sp);
-		this.setSize(300,300);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		this.setVisible(true);
+		
 	}
 
 }
